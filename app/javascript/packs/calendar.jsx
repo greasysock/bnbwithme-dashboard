581fa2019a3852cd45b26478dc5ad4a05ffd75fc
chris <chris.gresock@gmail.com>
2019-04-30 11:19:43 -0400

    Added initial react calendar views

--- app/javascript/packs/calendar.jsx
+++ app/javascript/packs/calendar.jsx
@@ -1,13 +1,50 @@
 import React from 'react'
+import ReactDom from 'react-dom'
 import FullCalendar from '@fullcalendar/react'
-import datGridPlugin from '@fullcalendar/daygrid'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import bootstrapPlugin from '@fullcalendar/bootstrap';
 
-export default class DemoApp extends React.Component {
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/bootstrap/main.css';
+import '@fullcalendar/daygrid/main.css';
+
+export default class EventCalendar extends React.Component {
 
     render() {
-      return (
-        <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
-      )
-    }
+        return (
+          <FullCalendar defaultView="dayGridMonth" 
+            plugins={[ dayGridPlugin, bootstrapPlugin ]}
+            events={[
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' },
+            { title: 'event 1', date: '2019-04-01' },
+            { title: 'event 2', date: '2019-04-02' }
+            ]}
+            themeSystem="bootstrap" 
+            />
+        )
+      }
   
   }
+
+  $(document).on('turbolinks:load', ReactDom.render(<EventCalendar/>, document.getElementById("calendar")) );
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'main/dashboard'
-  get 'main/calendar'
+  root 'main#dashboard'
+  get 'calendar', to: 'main#calendar'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
