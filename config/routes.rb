8d0938795da8a633dbd529fd4d8851fb8ee7dfa3
chris <chris.gresock@gmail.com>
2019-04-30 11:26:35 -0400

    Added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'main#dashboard'
   get 'calendar', to: 'main#calendar'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
