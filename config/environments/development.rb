ed51838f382703b9f0777844a1dfb6083406dc5e
chris <chris.gresock@gmail.com>
2019-05-06 14:05:56 -0400

    Changed harmony option to production -_-

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,8 +54,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.assets.js_compressor = Uglifier.new(harmony: true)
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
