174d798c88944c065011f77cc11649b6cb1222a9
chris <chris.gresock@gmail.com>
2019-05-06 17:59:27 -0400

    modified config

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,19 +7,6 @@ set :repo_url, "git@github.com:greasysock/bnbwithme-dashboard.git"
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-before "deploy:assets:precompile", "deploy:yarn_install"
-
-namespace :deploy do
-  desc 'Run rake yarn:install'
-  task :yarn_install do
-    on roles(:web) do
-      within release_path do
-        execute("cd #{release_path} && yarn install")
-      end
-    end
-  end
-end
-
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
 # Only keep the last 5 releases to save disk space
