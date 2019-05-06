e7b5948c2f0a2f9d49c822317b98495b8833cab5
chris <chris.gresock@gmail.com>
2019-05-06 19:50:19 -0400

    Modified styles

--- app/javascript/packs/calendar.jsx
+++ app/javascript/packs/calendar.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import ReservationList from "./components/ReservationList"
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/bootstrap/main.css';
-import '@fullcalendar/daygrid/main.css';
 
 class CalendarCard extends React.Component {
     render() {
