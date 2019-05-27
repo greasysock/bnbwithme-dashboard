90984a2fa905d918853191a18c86a847b254bf45
chris <chris.gresock@gmail.com>
2019-05-27 19:31:28 -0400

    Added persist state for calendar settings

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import throttle from 'lodash/throttle'
 
+import {loadState, saveState} from '../components/localStorage'
 import App from '../components/App'
 import reducers from '../reducers'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+const persistedState = loadState()
+const store = createStore(reducers, persistedState, composeEnhancers(applyMiddleware(thunk)))
+
+store.subscribe(throttle(()=> {
+    saveState({
+        calendarSettings: store.getState().calendarSettings
+    })
+}, 1000))
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.querySelector("#root"))
