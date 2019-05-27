abc63c0cada31551da73bfd16c0f8e778130400a
chris <chris.gresock@gmail.com>
2019-05-27 12:06:56 -0400

    Added organization to actions

---
+++ app/javascript/actions/propertyIcalsActions.jsx
@@ -0,0 +1,45 @@
+import humps from 'lodash-humps'
+import dehumps from 'snakecase-keys'
+import bnbwithme from '../api/bnbwithme'
+import {
+    FETCH_PROPERTY_ICALS,
+    FETCH_PROPERTY_ICAL,
+    UPDATE_PROPERTY_ICAL,
+    CREATE_PROPERTY_ICAL,
+    DESTROY_PROPERTY_ICAL,
+} from './types'
+import {_userHeaders} from '.'
+
+export const fetchPropertyIcals = (propertyId) => async (dispatch, getState) => {
+    const response = await bnbwithme.get(`/properties/${propertyId}/icals`, _userHeaders(getState))
+    dispatch({type:FETCH_PROPERTY_ICALS, payload: humps(response.data)})
+} 
+
+export const fetchPropertyIcal = (propertyId, id) => async (dispatch, getState) => {
+    const response = await bnbwithme.get(`/properties/${propertyId}/icals/${id}`, _userHeaders(getState))
+    dispatch({type:FETCH_PROPERTY_ICAL, payload: humps(response.data)})
+}
+
+export const updatePropertyIcal = (propertyId, id, formValues, callback = null) => async (dispatch, getState) => {
+    const response = await bnbwithme.put(`/properties/${propertyId}/icals/${id}`, dehumps({ical: formValues}), _userHeaders(getState))
+    dispatch({type:UPDATE_PROPERTY_ICAL, payload: humps(response.data)})
+    if(callback){
+        callback()
+    }
+} 
+
+export const destroyPropertyIcal = (propertyId, id, callback = null) => async (dispatch, getState) => {
+    await bnbwithme.delete(`/properties/${propertyId}/icals/${id}`, _userHeaders(getState))
+    dispatch({type:DESTROY_PROPERTY_ICAL, payload: {id, propertyId}})
+    if(callback){
+        callback()
+    }
+} 
+
+export const createPropertyIcal = (propertyId, formValues, callback = null) => async (dispatch, getState) => {
+    const response = await bnbwithme.post(`/properties/${propertyId}/icals`, dehumps({ical: formValues}), _userHeaders(getState))
+    dispatch({type:CREATE_PROPERTY_ICAL, payload: humps(response.data)})
+    if(callback){
+        callback()
+    }
+} 
---
+++ app/javascript/actions/reservationsActions.jsx
@@ -0,0 +1,39 @@
+import humps from 'lodash-humps'
+import _ from 'lodash'
+import bnbwithme from '../api/bnbwithme'
+
+import {
+    FETCH_PROPERTY_RESERVATIONS,
+    FETCH_RESERVATION,
+    ASSIGN_CLEANER_TO_RESERVATION,
+    REMOVE_CLEANER_FROM_RESERVATION,
+} from './types'
+
+import {_userHeaders} from '.'
+import {fetchProperties} from './propertiesActions'
+
+export const fetchPropertiesAndReservations = () => async (dispatch, getState) => {
+    await dispatch(fetchProperties())
+    const propertyIds = _.map(getState().properties, 'id')
+    const promises = []
+    propertyIds.forEach(id=>promises.push(dispatch(fetchPropertyReservations(id))))
+    await Promise.all(promises)
+}
+
+export const fetchPropertyReservations = propertyId => async (dispatch, getState) => {
+    const response = await bnbwithme.get(`/properties/${propertyId}/reservations`, _userHeaders(getState))
+    dispatch({type:FETCH_PROPERTY_RESERVATIONS, payload: humps(response.data)})
+}
+
+export const assignCleanerToReservation = (id, cleanerId, callback = null) => async (dispatch, getState) => {
+    const updatePayload = {reservation: {cleaner_id: cleanerId}}
+    const response = await bnbwithme.put(`/properties/reservations/${id}`, updatePayload, _userHeaders(getState))
+
+    dispatch({type: FETCH_RESERVATION, payload: humps(response.data)})
+
+    // Do Callback:
+    if(callback){
+        callback()
+    }
+
+}
