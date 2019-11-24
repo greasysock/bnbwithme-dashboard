3eb67b6558572000c76ec8e313f9eb99359265cb
chris <chris.gresock@gmail.com>
2019-11-24 11:56:26 -0500

    Optimized bundle size by dealing with lowest hanging fruit. Optimized lodash and moment

--- babel.config.js
+++ babel.config.js
@@ -44,6 +44,7 @@ module.exports = function(api) {
     ].filter(Boolean),
     plugins: [
       require('babel-plugin-macros'),
+      require('babel-plugin-lodash'),
       require('@babel/plugin-syntax-dynamic-import').default,
       isTestEnv && require('babel-plugin-dynamic-import-node'),
       require('@babel/plugin-transform-destructuring').default,
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,12 @@
 const { environment } = require('@rails/webpacker')
 const antdConfig = require('./antdConfig')
+const webpack = require('webpack')
 
 environment.config.merge(antdConfig)
+
+environment.plugins.prepend(
+  'MomentIgnoreLocales',
+  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+)
 
 module.exports = environment
